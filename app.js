65adff5e2e8f47c59a704f5ca562b4361c865b57
Obi <obi.netbits@outlook.com>
2016-05-27 01:42:00 +0000

    add route params

--- app.js
+++ app.js
@@ -14,6 +14,21 @@ app.get("/dog", function(req, res){
     res.send("MEOW");
 });
 
+//route variable/parameter
+app.get("/r/:subredditName", function(req, res) {
+    var subreddit = req.params.subredditName;
+    res.send("Welcome to the " + subreddit.toUpperCase() + " Subreddit!");
+});
+
+app.get("/r/:subredditName/comments/:id/:title/", function(req, res){
+    res.send("Welcome to the comments page");
+});
+
+// when you goto anyother link => "You are a Star"
+app.get("*", function(req, res){
+    res.send("You are a Star");
+});
+
 //Tell Express to listen for request(start over)
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("SERVER HAS STARTED!!!");
